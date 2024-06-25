5fcb41cb726a4d86f8955645c2b7fea15456e822
Matheus borges <matheusborges7526@gmail.com>
2024-06-25 17:27:31 -0300

    Implementei o repositório junto aos métodos presentes no controller "Categoria" para não deixar o tratamento de dados acoplado entre o AppContext e os controllers.
    OBS: próximo passo, implantar os métodos do repositório "CategoriaRepository" para o controller "Categoria".

---
+++ APIcatalogo/Repositories/CategoriaRepository.cs
@@ -0,0 +1,71 @@
+﻿using APIcatalogo.Context;
+using APIcatalogo.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace APIcatalogo.Repositories
+{
+    public class CategoriaRepository : ICategoriaRepository
+    {
+        private readonly AppDbContext _context;
+
+        public CategoriaRepository(AppDbContext context)
+        {
+            _context = context;
+        }
+
+        public IEnumerable<Categoria> GetCategorias()
+        {
+            var cat = _context.Categorias.ToList();
+            return cat;
+
+        }
+
+        public Categoria GetCateogria(int id)
+        {
+            var categoria = _context.Categorias.FirstOrDefault(x => x.CategoriaId == id);
+            return categoria;
+        }
+
+        public Categoria Create(Categoria categoria)
+        {
+            if(categoria == null) 
+            {
+             throw new ArgumentNullException(nameof(categoria));
+            
+            }
+            _context.Categorias.Add(categoria);
+            _context.SaveChanges();
+            return categoria;
+
+        }
+
+        public Categoria Update(int id, Categoria categoria)
+        {
+            var cat = _context.Categorias.FirstOrDefault(x=> x.CategoriaId==id);
+
+            if (id != categoria.CategoriaId)
+            {
+                throw new ArgumentNullException(nameof(categoria));
+
+            }
+            _context.Entry(categoria).State = EntityState.Modified;
+            _context.SaveChanges();
+            return cat;
+
+        }
+
+        public Categoria Delete(int id)
+        {
+            var categoria =_context.Categorias.Find(id);
+
+            if(categoria is null)
+            { throw new ArgumentNullException(nameof(categoria)); }
+
+            _context.Categorias.Remove(categoria);
+            _context.SaveChanges();
+            return categoria;
+        }
+
+        
+    }
+}
---
+++ APIcatalogo/Repositories/ICategoriaRepository.cs
@@ -0,0 +1,21 @@
+﻿using APIcatalogo.Models;
+using System.Reflection;
+using System.Runtime.InteropServices;
+
+namespace APIcatalogo.Repositories
+{
+
+    //interface permite varias implementacoes diferentes...
+    public interface ICategoriaRepository
+    {
+        IEnumerable<Categoria> GetCategorias();
+        
+        Categoria GetCateogria(int id);
+        
+        Categoria Create(Categoria categoria);
+        
+        Categoria Update(int id, Categoria categoria);
+        
+        Categoria Delete(int id);
+    }
+}
